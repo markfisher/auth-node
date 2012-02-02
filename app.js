379c2b4054dbcf2fc940c75d65fb859006e6045c
Mark Fisher <markfisher@vmware.com>
2012-02-02 18:16:34 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var cloudfoundry = require("cloudfoundry");
+var express = require("express");
+
+var users = {'larry': {username: 'larry', token: '123ABC', roles: ['employee']},
+  'curly': {username: 'curly', token: '456DEF', roles: ['employee', 'manager']},
+  'moe': {username: 'moe', token: '789GHI', roles: ['manager']}};
+var passwords = {'larry': 'larry', 'curly': 'curly', 'moe': 'moe'};
+
+var app = express.createServer();
+
+app.configure(function() {
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(app.router);
+});
+
+app.post("/authenticate", function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  if (!username || !password) { res.send(403); }
+  else if (!users[username]) { res.send(403); }
+  else if (password != passwords[username]) { res.send(403); }
+  else { res.send(users[username]); }
+});
+
+app.listen(cloudfoundry.getAppPort());
