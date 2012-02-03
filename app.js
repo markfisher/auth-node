89908642931d358b766cc9d6a872ac50a9b4f16a
Jeremy Grelle <jgrelle@vmware.com>
2012-02-02 16:25:32 -0800

    Added CORS support.

--- app.js
+++ app.js
@@ -14,9 +14,17 @@ app.configure(function() {
   app.use(app.router);
 });
 
+app.all("/*", function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.post("/authenticate", function(req, res) {
   var username = req.body.username;
   var password = req.body.password;
+  console.log("Received username: "+username);
+  console.log("Received password: "+password);
   if (!username || !password) { res.send(403); }
   else if (!users[username]) { res.send(403); }
   else if (password != passwords[username]) { res.send(403); }
